de0c35334480d2f87e522c230b0c9cbbccc5a341
Thiago Martins de Sousa <thiagodf@live.com>
2017-07-21 19:14:44 -0300

    Inserindo o modelo Grupo

---
+++ NAControl/NaControl.Domain/Models/Grupo.cs
@@ -0,0 +1,45 @@
+﻿using NAControl.Common.Resources;
+using NAControl.Common.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAControl.Domain.Models
+{
+    public class Grupo
+    {
+        #region Ctor
+
+        public Grupo(){ }
+
+        public Grupo(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Id { get; private set; }
+
+        public string Nome { get; private set; }
+
+        #endregion
+
+        #region Methods
+
+        public void AlteraNome(string nome)
+        {
+            this.Nome = nome;
+        }
+        public void Validar()
+        {
+            AssertionConcern.AssertArgumentLength(this.Nome, 3, 100, Errors.InvalidUserName);
+        }
+
+        #endregion
+    }
+}

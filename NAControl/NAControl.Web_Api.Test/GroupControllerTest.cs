b250d8490846e5e53169d918cecdc5b6babab1f9
Julianno Garcia <juliannotcg@hotmail.com>
2017-09-08 17:14:21 -0300

    Projeto de test

---
+++ NAControl/NAControl.Web_Api.Test/GroupControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NAControl.Web_Api.Test
+{
+    [TestClass]
+    public class GroupControllerTest
+    {
+        [TestMethod]
+        public void GetAllGroups()
+        {
+            var controller = new GroupController(_repository);
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            var result = controller.Get();
+
+            IEnumerable<Car> cars;
+
+            Assert.IsTrue(result.TryGetContentValue<IEnumerable<Car>>(out cars));
+            Assert.IsNotNull(cars);
+            Assert.AreEqual(3, cars.Count());
+        }
+    }
+}
---
+++ NAControl/NAControl.Web_Api.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NAControl.Web_Api.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NAControl.Web_Api.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("97cc3a5b-81c6-45a5-ba77-9b4abdc105eb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
